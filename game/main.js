9e1253470de9d20fb76413d1904da78cfb84ddc9
martin.dansc <martin.dansc@gmail.com>
2017-02-12 00:20:43 +0100

    integrated

--- game/main.js
+++ game/main.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var bodyParser = require("body-parser")
+var bodyParser = require("body-parser");
+
+var network = require('./perceptron');
 
 app.set('views', __dirname + "/views");
 app.engine('html', require('ejs').renderFile);
@@ -28,19 +30,40 @@ var players=[];
 var keys={};
 var positions={};
 var bullets=[];
-var playing=false;
+
+var playing = false;
 var players_actions = true;
 
+var player_Ai = {};
+
 io.on('connection', function(socket){
-   players.push(socket.id);
-	//Hello
+   	players.push(socket.id);
+	createPlayer(socket.id);
+	player_Ai[socket.id] = false;
+
 	socket.on('disconnect', function(){
 		delete players[socket.id];
 	});
 	socket.on('keyPressed', function(msg){
 		keys[socket.id]=msg;
 	});
-	socket.on('startGame', function(){
+	socket.on('startGame', function(aiPlayers){
+		if(aiPlayers != 'undefined' || aiPlayers != 'none') {
+			for(i in players) {
+				var id_play = players[i];
+				if(aiPlayers == 'all' || id_play != socket.id) {
+					player_Ai[id_play] = true;
+				}
+				else player_Ai[id_play] = false;
+			}
+		}
+		else {
+			for(i in players) {
+				var id_play = players[i];
+				player_Ai[id_play] = false;
+			}
+		}
+
 		if(players.length>1){
 			for (var i=0; i<2; ++i){
 				keys[socket.id]=0;
@@ -52,13 +75,23 @@ io.on('connection', function(socket){
 	});
 });
 	
-function mainloop(){
+function mainloop() {
 	if(!playing) return;
 	for(var i=0; i<2;++i){
 		var id_play=players[i];
 
 		//moure players
 		if(players_actions){
+
+			if(player_Ai[id_play]) {
+				var data = network.transformState(id_play, positions, bullets);
+				keys[id_play] = network.getAction(id_play,data);
+			}
+			else {
+				var data = network.transformState(id_play, positions, bullets);
+				network.trainNet(id_play, data, keys[id_play]);
+			}
+
 			//actualitzar posicions
 			if(keys[id_play]==0){ //recte
 				if(positions[id_play].d==0) positions[id_play].x++;
@@ -93,6 +126,7 @@ function mainloop(){
 			if(bullets[i].t==0) delete bullets[i];
 		}
 	}
+
 	//enviar estat
 	var obj = {positions_players: positions, positions_bullets: bullets};
 	io.emit('updateGame', obj);
@@ -116,8 +150,7 @@ function mainloop(){
 	}
 
 	//canviar si es mou el jugador o no
-	if(players_actions) players_actions=false;
-	else players_actions=true;
+	players_actions = !player_actions;
 }
 
 setInterval(mainloop,100);
