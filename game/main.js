3f84e381ba2ed60de73eb3646960cc2ffd63f805
mariavila <mariava.1213@gmail.com>
2017-02-12 11:00:04 +0100

    arreglar bug

--- game/main.js
+++ game/main.js
@@ -65,10 +65,10 @@ io.on('connection', function(socket){
 		io.emit('canvasVisible', '');
 		bullets=[];
 
-		if(aiPlayers.type != "" && aiPlayers.type != 'none') {
+		if(aiPlayers != "" && aiPlayers != 'none') {
 			for(i in players) {
 				var id_play = players[i];
-				if(aiPlayers.type == 'all' || id_play != socket.id) {
+				if(aiPlayers == 'all' || id_play != socket.id) {
 					player_Ai[id_play] = true;
 					network.startTraining(id_play);
 				}
